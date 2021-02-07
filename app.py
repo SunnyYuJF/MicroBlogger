7974be852f775ca942154e66181ea715124d3966
SunnyYuJF <jy63@rice.edu>
2021-02-07 15:23:56 -0700

    Update

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ def load_user(user_id):
 
 # blueprint for auth routes in our app
 from auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+app.register_blueprint(auth_blueprint)
 
 # blueprint for non-auth parts of app
 from main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+app.register_blueprint(main_blueprint)
 
 
 if __name__ == '__main__':
