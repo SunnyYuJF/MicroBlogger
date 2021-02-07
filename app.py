6b0e39fcb015d6f46c0391deab64c0023294716d
SunnyYuJF <jy63@rice.edu>
2021-02-07 14:57:16 -0700

    Update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '9OLWxND4o83j4K4iuopO')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 
     db.init_app(app)
--- db.py
+++ db.py
@@ -1,3 +1,15 @@
-from app import db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_login import LoginManager
+
+
+db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '9OLWxND4o83j4K4iuopO')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+
+db.init_app(app)
+
 
 db.create_all()
